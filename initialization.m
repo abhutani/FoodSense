d48b891c50614033151a5cbd7e9ee48fd402cfd2
abhutani <anirudh.bhutani@gmail.com>
2016-08-31 15:57:39 -0700

    moth mass added, need to add force vector

---
+++ initialization.m
@@ -0,0 +1,40 @@
+%______________________________________________________________________________________________
+%  Moth-Flame Optimization Algorithm (MFO)                                                            
+%  Source codes demo version 1.0                                                                      
+%                                                                                                     
+%  Developed in MATLAB R2011b(7.13)                                                                   
+%                                                                                                     
+%  Author and programmer: Seyedali Mirjalili                                                          
+%                                                                                                     
+%         e-Mail: ali.mirjalili@gmail.com                                                             
+%                 seyedali.mirjalili@griffithuni.edu.au                                               
+%                                                                                                     
+%       Homepage: http://www.alimirjalili.com                                                         
+%                                                                                                     
+%  Main paper:                                                                                        
+%  S. Mirjalili, Moth-Flame Optimization Algorithm: A Novel Nature-inspired Heuristic Paradigm, 
+%  Knowledge-Based Systems, DOI: http://dx.doi.org/10.1016/j.knosys.2015.07.006
+%_______________________________________________________________________________________________
+
+% This function creates the first random population of moths
+
+function X=initialization(SearchAgents_no,dim,ub,lb)
+
+Boundary_no= size(ub,2); % numnber of boundaries
+
+% If the boundaries of all variables are equal and user enter a signle
+% number for both ub and lb
+if Boundary_no==1
+    X(1)=rand(SearchAgents_no,dim).*(ub-lb)+lb;
+    X(2) = 1;
+end
+
+% If each variable has a different lb and ub
+if Boundary_no>1
+    for i=1:dim
+        ub_i=ub(i);
+        lb_i=lb(i);
+        X(1,i)=rand(SearchAgents_no,1).*(ub_i-lb_i)+lb_i;
+        X(2,i)=1;
+    end
+end
