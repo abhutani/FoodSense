92eff07efbc550491e243a88e535adf2ae5b0bee
abhutani <anirudh.bhutani@gmail.com>
2016-09-06 00:29:44 +0530

    working code, parameters need to be tuned

--- main.m
+++ main.m
@@ -1,35 +1,3 @@
-%______________________________________________________________________________________________
-%  Moth-Flame Optimization Algorithm (MFO)                                                            
-%  Source codes demo version 1.0                                                                      
-%                                                                                                     
-%  Developed in MATLAB R2011b(7.13)                                                                   
-%                                                                                                     
-%  Author and programmer: Seyedali Mirjalili                                                          
-%                                                                                                     
-%         e-Mail: ali.mirjalili@gmail.com                                                             
-%                 seyedali.mirjalili@griffithuni.edu.au                                               
-%                                                                                                     
-%       Homepage: http://www.alimirjalili.com                                                         
-%                                                                                                     
-%  Main paper:                                                                                        
-%  S. Mirjalili, Moth-Flame Optimization Algorithm: A Novel Nature-inspired Heuristic Paradigm, 
-%  Knowledge-Based Systems, DOI: http://dx.doi.org/10.1016/j.knosys.2015.07.006
-%_______________________________________________________________________________________________
-% You can simply define your cost in a seperate file and load its handle to fobj 
-% The initial parameters that you need are:
-%__________________________________________
-% fobj = @YourCostFunction
-% dim = number of your variables
-% Max_iteration = maximum number of generations
-% SearchAgents_no = number of search agents
-% lb=[lb1,lb2,...,lbn] where lbn is the lower bound of variable n
-% ub=[ub1,ub2,...,ubn] where ubn is the upper bound of variable n
-% If all the variables have equal lower bound you can just
-% define lb and ub as two single number numbers
-
-% To run MFO: [Best_score,Best_pos,cg_curve]=MFO(SearchAgents_no,Max_iteration,lb,ub,dim,fobj)
-%______________________________________________________________________________________________
-
 clear all 
 clc
 
